bc45e35632194b0badf7a8aa12155817dedfd119
Vic Samoylik <v.samojlik@gmail.com>
2023-11-25 19:31:47 +0300

    feat: style and plain components structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
-
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
 </script>
 
 <template>
-  <h1>Hello Vue</h1>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
