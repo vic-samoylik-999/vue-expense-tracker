7b401f71a05047ee548129e8a3fc0047d451ef1f
Vic Samoylik <v.samojlik@gmail.com>
2023-11-25 20:31:14 +0300

    feat: transaction list logic with dumb data

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,29 @@
+<script>
+export default {
+    data() {
+        return {
+            transactions: [
+                { id: 1, text: "Flower", amout: -20.00 },
+                { id: 2, text: "Paycheck", amout: 1120.00 },
+                { id: 3, text: "Shirt", amout: -20.00 },
+                { id: 4, text: "Grocery", amout: -20.00 },
+                { id: 5, text: "Shoes", amout: -20.00 },
+            ]
+        }
+    }
+}
+</script>
+
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li class="minus">
-            Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-            Paychek <span>$400</span><button class="delete-btn">x</button>
-        </li>
+        <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amout < 0 ? 'minus' : 'plus'">
+            {{ transaction.text }} <span>{{ transaction.amout }}</span><button class="delete-btn">x</button>
+        </li> <!-- <li class="minus">
+                    Cash <span>-$400</span><button class="delete-btn">x</button>
+                </li>
+                <li class="plus">
+                    Paychek <span>$400</span><button class="delete-btn">x</button>
+                </li> -->
     </ul>
 </template>
