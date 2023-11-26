4181af3e925f064f48b736178468d3210246fae0
Vic Samoylik <v.samojlik@gmail.com>
2023-11-26 20:19:56 +0300

    feat: save, load, update transactions with LS

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,18 @@ import Balance from './components/Balance.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { nanoid } from "nanoid"
 import { useToast } from "vue-toastification";
 
 // Dummy data
-let transactions = ref([
-  { id: nanoid(), text: "Flower", amount: -20.25 },
-  { id: nanoid(), text: "Paycheck", amount: 1120.00 },
-  { id: nanoid(), text: "Shirt", amount: -20.00 },
-  { id: nanoid(), text: "Grocery", amount: -20.50 },
-  { id: nanoid(), text: "Shoes", amount: -20.00 },
-])
+let transactions = ref([])
+
+// Check LocalStorage
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem('transactions'))
+  if (savedTransactions) transactions.value = savedTransactions
+})
 
 // Get total
 const total = computed(() => {
@@ -46,21 +46,28 @@ const expenses = computed(() => {
     .toFixed(2)
 })
 
+// Add new transaction
 const handleTransactionSubmitted = (transactionData) => {
   transactions.value.push({
     id: nanoid(),
     text: transactionData.text,
     amount: transactionData.amount,
   })
+  saveTransactionsLocacStorage()
 }
 
+// Delete transaction
 const handleTransactionDelete = (id) => {
   const toast = useToast();
-  toast.info(`The item: "${transactions.value.find(item => item.id === id).text}" was removed...`)
+  toast.success(`The "${transactions.value.find(item => item.id === id).text}" was removed...`)
   transactions.value = transactions.value.filter(item => item.id !== id)
-
+  saveTransactionsLocacStorage()
 }
 
+// Save transaction to LS
+const saveTransactionsLocacStorage = () => {
+  localStorage.setItem('transactions', JSON.stringify(transactions.value))
+}
 </script>
 
 <template>
